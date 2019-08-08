502236003f09c25b48a56df16c7139aee2d5dad8
dlwjd726 <eqie112@naver.com>
2019-08-08 15:11:13 +0900

    props를 통해 컴포넌트에게 값 전달

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function Hello() {
-  return <div>Hello React</div>;
+function Hello({ color, name }) {
+  return (
+    <div
+      style={{
+        color
+      }}
+    >
+      안녕하세요 {name}
+    </div>
+  );
 }
+
+Hello.defaultProps = {
+  name: "No Name"
+};
 
 export default Hello;
